1f5212f4b12b2fc68798526157633a288c0d467f
ArturoBasa <zs24013319@estudiantes.uv.mx>
2026-05-01 21:41:07 -0600

    adelanto de vista principal y controlador de vista principal

---
+++ src/proyectobd2/controlador/GUIPrincipalControlador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectobd2.controlador;
+import proyectobd2.vista.GUIPrincipal;
+/**
+ *
+ * @author basa2
+ */
+public class GUIPrincipalControlador {
+    
+}
